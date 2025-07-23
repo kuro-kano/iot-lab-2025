39562a01a8ab44dad709192c6607877b708c9da0
Gie.kaano <66070091@kmitl.ac.th>
2025-07-23 17:47:31 +0700

    add: db/drizzle.ts and db/schema.ts what create table students

---
+++ Week-1-RestAPI-with-Hono/api/db/schema.ts
@@ -0,0 +1,11 @@
+import { relations } from 'drizzle-orm';
+import * as t from 'drizzle-orm/pg-core';
+
+export const students = t.pgTable('students', {
+    id: t.bigserial({ mode: 'number' }).primaryKey(),
+    firstname: t.varchar({ length: 30 }).notNull(),
+    lastname: t.varchar({ length: 30 }).notNull(),
+    student_id: t.varchar({ length: 10 }).notNull().unique(),
+    birthday: t.varchar({ length: 50 }),
+    gender: t.varchar({ length: 10 }),
+});
