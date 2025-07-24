b393d295018e2af9f5604067b18eeef2e61d403f
Ka-no <138846884+kuro-kano@users.noreply.github.com>
2025-07-24 15:37:39 +0700

    add: all other routes/ file and config src/db a chotto

--- Week-2-RestAPI-with-React/Backend/src/db/schema.ts
+++ Week-2-RestAPI-with-React/Backend/src/db/schema.ts
@@ -1,26 +1,29 @@
 import { relations } from "drizzle-orm";
 import * as t from "drizzle-orm/sqlite-core";
-import { id } from "zod/locales";
 
 export const genres = t.sqliteTable("genres", {
-  id: t.integer().primaryKey({ autoIncrement: true }),
+  id: t.integer().primaryKey({
+    autoIncrement: true,
+  }),
   title: t.text().notNull(),
 });
 
 export const books = t.sqliteTable("books", {
-  id: t.integer().primaryKey({ autoIncrement: true }),
+  id: t.integer().primaryKey({
+    autoIncrement: true,
+  }),
   title: t.text().notNull(),
   author: t.text().notNull(),
   publishedAt: t.integer().notNull(),
 
-  genresId: t.integer().notNull().references(() => genres.id, {
+  genreId: t.integer().references(() => genres.id, {
     onDelete: "set null",
   }),
 });
 
 export const bookRelations = relations(books, ({ one }) => ({
   genre: one(genres, {
-    fields: [books.genresId],
+    fields: [books.genreId],
     references: [genres.id],
   }),
 }));
