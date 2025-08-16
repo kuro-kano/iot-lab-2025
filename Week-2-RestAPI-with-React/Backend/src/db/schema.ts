e03860fe10fb7c5d5ec885a701bf4c269b2c533e
Gie.kaano <66070091@kmitl.ac.th>
2025-08-16 16:09:07 +0700

    modify: remove unused table and add: orders table to collect an order from customer

--- Week-2-RestAPI-with-React/Backend/src/db/schema.ts
+++ Week-2-RestAPI-with-React/Backend/src/db/schema.ts
@@ -1,13 +1,6 @@
 import { relations } from "drizzle-orm";
 import * as t from "drizzle-orm/sqlite-core";
 
-export const genres = t.sqliteTable("genres", {
-  id: t.integer().primaryKey({
-    autoIncrement: true,
-  }),
-  title: t.text().notNull(),
-});
-
 export const books = t.sqliteTable("books", {
   id: t.integer().primaryKey({
     autoIncrement: true,
@@ -15,24 +8,15 @@ export const books = t.sqliteTable("books", {
   title: t.text().notNull(),
   author: t.text().notNull(),
   publishedAt: t.integer().notNull(),
-
-  genreId: t.integer().references(() => genres.id, {
-    onDelete: "set null",
-  }),
 });
 
-export const staffs = t.sqliteTable("staffs", {
-  staff_id: t.integer().primaryKey({
+export const orders = t.sqliteTable("orders", {
+  id: t.integer().primaryKey({
     autoIncrement: true,
   }),
-  firstname: t.text().notNull(),
-  lastname: t.text().notNull(),
-})
-
-
-export const bookRelations = relations(books, ({ one }) => ({
-  genre: one(genres, {
-    fields: [books.genreId],
-    references: [genres.id],
-  }),
-}));
+  menu: t.text().notNull(),
+  quantity: t.integer().notNull(),
+  price: t.integer().notNull(),
+  status: t.text().notNull(),
+  ordered_at: t.integer().notNull(),
+});
